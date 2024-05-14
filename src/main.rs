67ea015dd523de5e235dfa3d154467e743e6fbc6
wwwhoami <subman450@gmail.com>
2024-05-14 20:57:36 +0300

    bind to a port

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    const HOST: &str = "127.0.0.1";
+    const PORT: i32 = 4221;
+    let addr = format!("{}:{}", HOST, PORT);
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind(addr).unwrap();
+
+    println!("server listening on port {}", PORT);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!(
+                    "accepted new connection from: {}",
+                    stream.peer_addr().unwrap()
+                );
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
