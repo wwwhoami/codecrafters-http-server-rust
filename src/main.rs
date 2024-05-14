a66f1ae92e2b3145f987e66b34d2f4346735050c
wwwhoami <subman450@gmail.com>
2024-05-14 21:08:35 +0300

    respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     const HOST: &str = "127.0.0.1";
@@ -11,11 +11,22 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 println!(
                     "accepted new connection from: {}",
                     stream.peer_addr().unwrap()
                 );
+                let res = b"HTTP/1.1 200 OK\r\n";
+                let write_result = stream.write(res);
+
+                match write_result {
+                    Ok(_) => {
+                        println!("response sent");
+                    }
+                    Err(e) => {
+                        println!("error sending response: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
